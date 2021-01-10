d7c3be27cb7a174b07e16f66abf05b4cfb181033
= <=>
2021-01-10 15:41:53 -0300

    PUT /orders/{id}/delivered

--- backend/src/main/java/br/com/matheuspadilha/dsdelivery/controllers/OrderController.java
+++ backend/src/main/java/br/com/matheuspadilha/dsdelivery/controllers/OrderController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.websocket.server.PathParam;
 import java.net.URI;
 import java.util.List;
 
@@ -28,5 +29,11 @@ public class OrderController {
         dto = service.insert(dto);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
         return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping("/{id}/delivered")
+    public ResponseEntity<OrderDTO> setDelivered(@PathVariable Long id) {
+        OrderDTO dto = service.setDelivered(id);
+        return ResponseEntity.ok().body(dto);
     }
 }
